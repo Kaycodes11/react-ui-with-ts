befdc52e67e55e2752c37dc3123bd9acc596a409
Kaycodes11 <kayydraws@gmail.com>
2022-07-04 19:14:19 +0530

    learning the react hook form v7

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
-import {useForm, Resolver, SubmitHandler, useWatch, Control} from "react-hook-form";
+import {useForm, Resolver, SubmitHandler, useWatch, Control, useFieldArray} from "react-hook-form";
 import {yupResolver} from "@hookform/resolvers/yup";
 import * as yup from "yup";
-import {IntervieweeForm} from "./styles";
+
+enum Gender {
+    Male = `Male`,
+    Female = `Female`,
+    Others = `Others`
+}
 
 type FormValues = {
     firstName: string;
@@ -11,11 +16,12 @@ type FormValues = {
     age?: number;
     mobile: string;
     scheduleAt?: string;
-    gender: `Male` | `Female` | `others`;
-    experience: `fresher` | `experienced`;
+    gender:  Gender;
+    experience: `Fresher` | `Experienced`;
     stack?: `MEAN` | `MERN`
     expectedSalary?: string;
     isDeveloper: boolean | string;
+    pets?: { name: string }[]
 
 };
 
@@ -48,9 +54,16 @@ function IsolateReRender({control}: { control: Control<FormValues> }) {
 function Form() {
     // const {register, handleSubmit, watch, formState: {errors}} = useForm({resolver: yupResolver(Schema)});
     // const {register, handleSubmit, watch, formState: {errors}} = useForm<FormValues>({resolver});
-    const {register, watch, control, handleSubmit} = useForm<FormValues>();
+    const {register, watch, control, handleSubmit, formState: {errors, isValid}} = useForm<FormValues>({
+        mode: `onChange`,
+        delayError: 500,
+        defaultValues: { firstName: `Son`, surName: `Goku`, age: 18, gender: Gender.Male, pets: [] }
+    });
+    const {fields, append, prepend} = useFieldArray({control, name: "pets"})
 
     console.log(watch('firstName'));
+    console.log(errors);
+
 
     // const onSubmit = handleSubmit((data) => console.log(data))
     // const onSubmit: SubmitHandler<FormValues> = data => console.log(data);
@@ -59,24 +72,33 @@ function Form() {
     return (
         <form onSubmit={onSubmit}>
             <label htmlFor="firstname">FirstName:</label>
-            <input {...register("firstName")} id={"firstName"}/>
+            <input {...register("firstName", { required: true, minLength: 4})} id={"firstName"}/>
+            <p>{errors?.firstName?.message}</p>
 
             <label htmlFor="surName">SurName:</label>
-            <input {...register("surName")} id={"surName"} />
+            <input {...register("surName", { required: true })} id={"surName"} />
+            <p>{errors?.surName?.message}</p>
 
             <label htmlFor="age">Age:</label>
-            <input type={"number"} {...register("age")} id={"age"} />
+            <input type={"number"} {...register("age", {valueAsNumber: true})} id={"age"} />
 
             <label htmlFor="gender">Gender:</label>
             {/*<select name={"gender"} id={"gender"}>*/}
             <select {...register("gender")} id={"gender"}>
                 <option value={""}>Select....</option>
-                <option value={"male"}>Male</option>
-                <option value={"female"}>Female</option>
-                <option value={"others"}>Others</option>
+                <option value={Gender.Male}>{Gender.Male}</option>
+                <option value={Gender.Female}>{Gender.Female}</option>
+                <option value={Gender.Others}>{Gender.Others}</option>
             </select>
-
-            <button type={"submit"}>Start Interview</button>
+            <p>Pets</p>
+            <div>
+            {fields.map((field, index) => {
+                return <input key={field.id} {...register(`pets.${index}.name`, {required: true})} />
+            })}
+            </div>
+            <button type={"button"} onClick={()=> append({name: "append"})}>append</button>
+            <button type={"button"} onClick={() => prepend({name: "prepend"})}>prepend</button>
+            <button type={"submit"} disabled={!isValid}>Start Interview</button>
         </form>
     )
 
--- src/components/Form/styles.tsx
+++ src/components/Form/styles.tsx
@@ -1,18 +1,9 @@
 import styled from 'styled-components';
 
-export const IntervieweeForm = styled.div`
-  background-color: lightgreen;
-  display: flex;
-  flex-direction: row;
-  width: 400px;
-  height: 400px;
+export const FormSection = styled.div`
+  flex-direction: column;
   margin-top: 12px;
   padding: 4rem;
-  border: 2px solid green;
-  justify-content: center;
   align-content: center;
-  
 `;
-export const Label = styled.label``;
-export const FormElement = styled.input``;
 
