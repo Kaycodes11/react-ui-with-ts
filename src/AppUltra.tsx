7df74b15dbfd5eb36781a4c88f142fe498d4119e
Kaycodes11 <kayydraws@gmail.com>
2022-07-10 18:30:18 +0530

    footer

--- src/AppUltra.tsx
+++ src/AppUltra.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import {NavBar} from "./components/NavBar4";
+import {Footer} from './components/Footer';
 import {GlobalStyle} from "./styles/global";
 import {Home} from "./pages/Home";
 
@@ -8,11 +9,12 @@ import {Home} from "./pages/Home";
 export default function App() {
     return (
         <Router>
-            <GlobalStyle />
-            <NavBar />
+            <GlobalStyle/>
+            <NavBar/>
             <Routes>
                 <Route index element={<Home/>}/>
             </Routes>
+            <Footer/>
         </Router>
     )
 }
--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,3 +1,99 @@
-const Footer = () => <div>Footer</div>
+import React from 'react';
+import {Button} from '../../styles/global';
+import {
+    FooterContainer, FooterLink,
+    FooterLinksContainer, FooterLinksItems, FooterLinksWrapper, FooterLinkTitle,
+    FooterSubHeading,
+    FooterSubscription,
+    FooterSubText,
+    Form,
+    FormInput, SocialIcon, SocialIconLink, SocialIcons, SocialLogo, SocialMedia, SocialMediaWrap, WebsiteRights
+} from "./styled";
+import {FaFacebook, FaInstagram, FaLinkedin, FaTwitter, FaYoutube} from "react-icons/fa";
 
-export {Footer};
+export const Footer: React.FC<FooterProps> = () => {
+    return (
+        <FooterContainer>
+            <FooterSubscription>
+                <FooterSubHeading>
+                    Join our exclusive membership to receive the latest news and updates
+                </FooterSubHeading>
+                <FooterSubText>You can unsubscribe at anytime</FooterSubText>
+                <Form>
+                    <FormInput name="email" type="email" placeholder="Your email"/>
+                    <Button fontBig>Subscribe</Button>
+                </Form>
+            </FooterSubscription>
+            <FooterLinksContainer>
+                <FooterLinksWrapper>
+
+                    <FooterLinksItems>
+                        <FooterLinkTitle>Contact Us</FooterLinkTitle>
+                        <FooterLink to="/sign-up">How it works</FooterLink>
+                        <FooterLink to="/">Testimonials</FooterLink>
+                        <FooterLink to="/">Careers</FooterLink>
+                        <FooterLink to="/">Investors</FooterLink>
+                        <FooterLink to="/">Terms of Service</FooterLink>
+                    </FooterLinksItems>
+
+                    <FooterLinksItems>
+                        <FooterLinkTitle>Videos</FooterLinkTitle>
+                        <FooterLink to="/sign-up">How it works</FooterLink>
+                        <FooterLink to="/">Testimonials</FooterLink>
+                        <FooterLink to="/">Careers</FooterLink>
+                        <FooterLink to="/">Investors</FooterLink>
+                        <FooterLink to="/">Terms of Service</FooterLink>
+                    </FooterLinksItems>
+
+                    <FooterLinksItems>
+                        <FooterLinkTitle>Social Media</FooterLinkTitle>
+                        <FooterLink to="/sign-up">How it works</FooterLink>
+                        <FooterLink to="/">Testimonials</FooterLink>
+                        <FooterLink to="/">Careers</FooterLink>
+                        <FooterLink to="/">Investors</FooterLink>
+                        <FooterLink to="/">Terms of Service</FooterLink>
+                    </FooterLinksItems>
+
+                </FooterLinksWrapper>
+            </FooterLinksContainer>
+
+            <SocialMedia>
+                <SocialMediaWrap>
+                    <SocialLogo to={""}>
+                        <SocialIcon/>
+                        ULTRA
+                    </SocialLogo>
+                    <WebsiteRights>ULTRA 2022</WebsiteRights>
+                    <SocialIcons>
+                        <SocialIconLink href={""} target={"_blank"} aria-label={"Twitter"}>
+                            <FaTwitter/>
+                        </SocialIconLink>
+
+                        <SocialIconLink href={""} target={"_blank"} aria-label={"Facebook"}>
+                            <FaFacebook/>
+                        </SocialIconLink>
+
+                        <SocialIconLink href={""} target={"_blank"} aria-label={"Instagram"}>
+                            <FaInstagram/>
+                        </SocialIconLink>
+
+                        <SocialIconLink href={"#"} target={"_blank"} aria-label={"YouTube"} rel="noopener noreferrer">
+                            <FaYoutube/>
+                        </SocialIconLink>
+
+                        <SocialIconLink href={"https://www.linkedin.com/in/pallab-kayal-838a24a2/"} target={"_blank"}
+                                        aria-label={"LinkedIn"}>
+                            <FaLinkedin/>
+                        </SocialIconLink>
+
+                    </SocialIcons>
+                </SocialMediaWrap>
+            </SocialMedia>
+        </FooterContainer>
+    )
+
+}
+
+interface FooterProps {
+}
+
